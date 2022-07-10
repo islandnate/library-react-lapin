1043ade7e3a8c8386790651bdb5b4e3841dac7b1
islandnate <pontenate@gmail.com>
2022-07-09 21:39:20 -0700

    creating my ecommerce page in react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import Footer from './components/Footer';
+import React from "react"
+import Nav from './components/Nav'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import Books from "./pages/Books"
+import { books } from "./data"
+import BookInfo from './pages/BookInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Nav />
+        <Route path="/" exact component={Home} />
+        <Route path="/books" exact render={() => <Books books={books} />} />
+        <Route path="/books/1" exact render={() => <BookInfo books={books} />} />
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
